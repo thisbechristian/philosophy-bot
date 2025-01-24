433d6e058d53f0b4a4a8ff67bd7fe52da4773267
Christian Boni <christianjboni@gmail.com>
2025-01-24 16:20:13 -0500

    adding logic to send emails

--- app.js
+++ app.js
@@ -1,23 +1,49 @@
+// Imports
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const nodemailer = require('nodemailer');
 
-// Constants
+// AI Options
 const model = "gemini-2.0-flash-exp";
 const generationConfig = { temperature: 2, responseMimeType: "text/plain", };
 const prompt = "Hello, how are you today?";
 
+// Email Options
+const fromEmail = process.env.EMAIL;
+const email = {
+    from: fromEmail,
+    to: ["christianjboni@gmail.com"]
+};
+
 // Create AI Client 
-const apiKey = process.env.GOOGLEAI_API_KEY;
-const googleAI = new GoogleGenerativeAI(apiKey);
+const googleAI = new GoogleGenerativeAI(process.env.GOOGLEAI_API_KEY);
 const ai = googleAI.getGenerativeModel({ model });
 
+// Create Email Client
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: fromEmail,
+        pass: process.env.EMAIL_PASSWORD,
+    },
+});
+
 const run = async () => {
+    let aiResponse = "";
     try {
         // Prompt AI
         const aiChatSession = ai.startChat({ generationConfig, history: [], });
         const result = await aiChatSession.sendMessage(prompt);
-        console.log(result.response.text());
+        aiResponse = result.response.text;
     } catch (error) {
-        console.log(error);
+        console.log("Failed to prompt AI: ", error);
+    }
+    try {
+        // Send Email
+        email.subject = "";
+        email.html = aiResponse;
+        await transporter.sendMail(email);
+    } catch (error) {
+        console.log("Failed to send Emai: ", error);
     }
 }
 
